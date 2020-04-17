72fd1d5fc5704938a152200fb9a506f4f805d2da
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-04-17 13:12:03 +0100

    Use glob to list files and re to pull list of years from filenames.

--- processing/load_xls.py
+++ processing/load_xls.py
@@ -1,1 +1,13 @@
 import xlrd
+from glob import glob
+import re
+
+folder = "..\\data\\CCG_populations\\"
+files = glob(f"{folder}*.xls*")
+
+filenames = [file.replace(folder, "") for file in files]
+print(filenames)
+
+pattern = re.compile(r'\d\d\d\d')
+years = [pattern.findall(filename)[0] for filename in filenames]
+print(years)
