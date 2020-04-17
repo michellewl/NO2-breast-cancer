2a1611d0461538c7f63865e29bf2e573e2d4325b
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-04-17 14:03:32 +0100

    Defined function to load Excel file and specify sheet to load as pandas dataframe.

--- processing/load_xls.py
+++ processing/load_xls.py
@@ -1,13 +1,37 @@
-import xlrd
 from glob import glob
 import re
+import xlrd
+import pandas as pd
 
 folder = "..\\data\\CCG_populations\\"
-files = glob(f"{folder}*.xls*")
+filepaths = glob(f"{folder}*.xls*")
 
-filenames = [file.replace(folder, "") for file in files]
-print(filenames)
+# pattern = re.compile(r'\d\d\d\d')
+# years = [pattern.findall(file)[0] for file in filepaths]
+# print(years)
 
-pattern = re.compile(r'\d\d\d\d')
-years = [pattern.findall(filename)[0] for filename in filenames]
-print(years)
+recent_years = []
+for file in filepaths:
+    if "2002" in file:
+        continue
+    else:
+        recent_years.append(file)
+
+def load_df_from_xlsheet(filename, sheet_name_pattern):
+    """
+    Function to load a pandas dataframe object from a Microsoft Excel spreadsheet file with specified sheet.
+    :param filename: string object with full Excel file path
+    :param sheet_name_pattern: re compiler object for finding desired sheet in Excel file
+    :return: pandas dataframe object
+    """
+    workbook = xlrd.open_workbook(filename)
+    sheets = workbook.sheet_names()
+
+    for sheet in sheets:
+        if sheet_name_pattern.findall(sheet):
+            required_sheet = sheet
+
+    return pd.read_excel(filename, required_sheet)
+
+df = load_df_from_xlsheet(recent_years[0], re.compile(r"\wemale"))
+print(df)
