7f29ddc9df3553d8c7608e55c578bce1241874b3
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-04-20 23:33:46 +0100

    Added verbose parameter to data import function to print sheet name.

--- processing/CCG_popns_functions.py
+++ processing/CCG_popns_functions.py
@@ -3,12 +3,13 @@ import re
 import xlrd
 import pandas as pd
 
-def load_df_from_xlsheet(filename, sheet_name_pattern):
+def load_df_from_xlsheet(filename, sheet_name_pattern, verbose=False):
     """
     Function to load a pandas dataframe object from a Microsoft Excel spreadsheet file with specified sheet.
 
     :param filename: string object with full Excel file path
     :param sheet_name_pattern: re compiler object for finding desired sheet in Excel file
+    :param verbose: boolean value to choose whether to print sheet name
     :return: pandas dataframe object
     """
     workbook = xlrd.open_workbook(filename)
@@ -17,6 +18,9 @@ def load_df_from_xlsheet(filename, sheet_name_pattern):
     for sheet in sheets:
         if sheet_name_pattern.findall(sheet):
             required_sheet = sheet
+
+    if verbose:
+        print(f"Sheet name: {required_sheet}")
 
     return pd.read_excel(filename, required_sheet)
 
