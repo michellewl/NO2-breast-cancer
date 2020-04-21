c36bd3e98a4f1f151aaff08c5e68ee1ce0af55e1
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-04-22 00:12:03 +0100

    Code to join all years dataframes to full_df.

--- processing/CCG_populations.py
+++ processing/CCG_populations.py
@@ -25,52 +25,7 @@ for file in filepaths:
     else:
         years_2012_to_18.append(file)
 
-# Dealing with 2011 (data has different formatting).
-if year == "2011" or year == "all":
-    filename = year_2011[0]
-    year = re.compile(r"\d\d\d\d").findall(filename)[0]
-    df = fn.load_df_from_xlsheet(filename, re.compile(r"\wemale"))
-    col_names = df.copy().columns.tolist()
-
-    london_df = fn.get_area_df(df, "Area Name", "London", "London")
-
-    ccg_col = col_names[col_names.index("Area Name")+2]
-    london_ccg_df = london_df.loc[london_df[ccg_col].notna()].drop(columns=["Area Name", col_names[col_names.index("Area Name")+1]])
-    london_ccg_df.rename(columns= {"Area Code":"area_code", "All Ages":"all_ages", ccg_col:"ccg", "90+":90},inplace=True)
-    london_ccg_df = fn.group_ages(london_ccg_df, 0, 40)
-    london_ccg_df = fn.group_ages(london_ccg_df, 40, 71)
-    london_ccg_df = fn.group_ages(london_ccg_df, 71, 91)
-
-    london_ccg_df = fn.rename_age_cols_with_year(london_ccg_df, "2011")
-    print(f"Filename: {filename}\nNumber of CCGs: {len(london_ccg_df)}")
-    # print(f"{london_ccg_df.head(2)}")
-    # print(london_ccg_df.columns)
-
-# Dealing with 2012 onwards (data files have same formatting).
-if year=="2012-18" or year=="all":
-    for i in range(len(years_2012_to_18)):
-        filename = years_2012_to_18[i]
-        year = re.compile(r"\d\d\d\d").findall(filename)[0]
-
-        df = fn.load_df_from_xlsheet(filename, re.compile(r"\wemale"))
-        df = fn.set_new_header(df, "Area Codes ")
-        col_names = df.copy().columns.tolist()
-        london_df = fn.get_area_df(df, 1, "London", "NHS England London")
-
-        col_names[1] = "Unknown1"
-        col_names[2] = "Unknown2"
-        london_df.columns = col_names
-
-        ccg_col = "Area Names"
-        london_ccg_df = london_df.loc[london_df[ccg_col].notna()].drop(columns=["Unknown1", "Unknown2"])
-        london_ccg_df.rename(columns= {"Area Codes ":"area_code", "All Ages":"all_ages", ccg_col:"ccg", "90+":90},inplace=True)
-        london_ccg_df = fn.group_ages(london_ccg_df, 0, 40)
-        london_ccg_df = fn.group_ages(london_ccg_df, 40, 71)
-        london_ccg_df = fn.group_ages(london_ccg_df, 71, 91)
-        london_ccg_df = fn.rename_age_cols_with_year(london_ccg_df, year)
-        print(f"Filename: {filename}\nNumber of CCGs: {len(london_ccg_df)}")
-        #print(f"{london_ccg_df.head(2)}")
-        print(london_ccg_df.columns)
+full_df = pd.DataFrame()
 
 # Dealing with 2002-2010 (different formatting)
 if year == "2002-10" or year == "all":
@@ -115,6 +70,71 @@ if year == "2002-10" or year == "all":
         print(f"Sheet: {required_sheets[i]}\nNumber of CCGs: {len(london_ccg_df)}")
         #print(london_ccg_df.head(2))
         #print(london_ccg_df.columns)
+        london_ccg_df.set_index(keys = ["area_code", "ccg"], inplace=True)
+
+        if full_df.empty:
+            full_df = london_ccg_df.copy()
+        else:
+            full_df.join(london_ccg_df, how="left")
+
+# Dealing with 2011 (data has different formatting).
+if year == "2011" or year == "all":
+    filename = year_2011[0]
+    year = re.compile(r"\d\d\d\d").findall(filename)[0]
+    df = fn.load_df_from_xlsheet(filename, re.compile(r"\wemale"))
+    col_names = df.copy().columns.tolist()
+
+    london_df = fn.get_area_df(df, "Area Name", "London", "London")
+
+    ccg_col = col_names[col_names.index("Area Name")+2]
+    london_ccg_df = london_df.loc[london_df[ccg_col].notna()].drop(columns=["Area Name", col_names[col_names.index("Area Name")+1]])
+    london_ccg_df.rename(columns= {"Area Code":"area_code", "All Ages":"all_ages", ccg_col:"ccg", "90+":90},inplace=True)
+    london_ccg_df = fn.group_ages(london_ccg_df, 0, 40)
+    london_ccg_df = fn.group_ages(london_ccg_df, 40, 71)
+    london_ccg_df = fn.group_ages(london_ccg_df, 71, 91)
+
+    london_ccg_df = fn.rename_age_cols_with_year(london_ccg_df, "2011")
+    print(f"Filename: {filename}\nNumber of CCGs: {len(london_ccg_df)}")
+    # print(f"{london_ccg_df.head(2)}")
+    # print(london_ccg_df.columns)
+    london_ccg_df.set_index(keys=["area_code", "ccg"], inplace=True)
+    if full_df.empty:
+        full_df = london_ccg_df.copy()
+    else:
+        full_df.join(london_ccg_df, how="left")
+
+# Dealing with 2012 onwards (data files have same formatting).
+if year=="2012-18" or year=="all":
+    for i in range(len(years_2012_to_18)):
+        filename = years_2012_to_18[i]
+        year = re.compile(r"\d\d\d\d").findall(filename)[0]
+
+        df = fn.load_df_from_xlsheet(filename, re.compile(r"\wemale"))
+        df = fn.set_new_header(df, "Area Codes ")
+        col_names = df.copy().columns.tolist()
+        london_df = fn.get_area_df(df, 1, "London", "NHS England London")
+
+        col_names[1] = "Unknown1"
+        col_names[2] = "Unknown2"
+        london_df.columns = col_names
+
+        ccg_col = "Area Names"
+        london_ccg_df = london_df.loc[london_df[ccg_col].notna()].drop(columns=["Unknown1", "Unknown2"])
+        london_ccg_df.rename(columns= {"Area Codes ":"area_code", "All Ages":"all_ages", ccg_col:"ccg", "90+":90},inplace=True)
+        london_ccg_df = fn.group_ages(london_ccg_df, 0, 40)
+        london_ccg_df = fn.group_ages(london_ccg_df, 40, 71)
+        london_ccg_df = fn.group_ages(london_ccg_df, 71, 91)
+        london_ccg_df = fn.rename_age_cols_with_year(london_ccg_df, year)
+        print(f"Filename: {filename}\nNumber of CCGs: {len(london_ccg_df)}")
+        #print(f"{london_ccg_df.head(2)}")
+        #print(london_ccg_df.columns)
+        london_ccg_df.set_index(keys = ["area_code", "ccg"], inplace=True)
+        if full_df.empty:
+            full_df = london_ccg_df.copy()
+        else:
+            full_df.join(london_ccg_df, how="left")
+
+print(full_df.columns)
 
 
 
