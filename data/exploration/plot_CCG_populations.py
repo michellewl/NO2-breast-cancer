f4a3346e508dc9aeca0f86efabdfe5ddabad726f
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-04-23 18:38:13 +0100

    Compressed code for plotting CCG populations.

--- data/exploration/plot_CCG_populations.py
+++ data/exploration/plot_CCG_populations.py
@@ -16,18 +16,15 @@ df = pd.read_csv(os.path.join(folder, filename), index_col=["area_code", "ccg"])
 age_groupings = ["all_ages", "below_age_40", "age_40_to_70", "above_age_70"]
 
 for group in age_groupings:
-
-    grouped_ages_columns = [column for column in df.columns if re.compile(rf"\d\d\d\d_{group}").match(column)]
-    grouped_ages_df = df[grouped_ages_columns]
+    grouped_ages_df = df[[column for column in df.columns if re.compile(rf"\d\d\d\d_{group}").match(column)]]
     grouped_ages_df.columns = [int(col.replace(f"_{group}","")) for col in grouped_ages_df.columns]
 
-    years = np.repeat(grouped_ages_df.columns.tolist(), len(df))
-    plot_df = pd.DataFrame(years, columns=["year"])
+    plot_df = pd.DataFrame(np.repeat(grouped_ages_df.columns.tolist(), len(df)), columns=["year"])
     plot_df["ccg"] = df.index.levels[1].tolist()*len(grouped_ages_df.columns)
-    plot_df["population"] = np.nan
+    #plot_df["population"] = np.nan
     plot_df = plot_df.sort_values(["ccg", "year"])
-    populations = [grouped_ages_df.loc[grouped_ages_df.index.levels[1] == ccg].values[0] for ccg in grouped_ages_df.index.levels[1]]
-    plot_df["population"] = np.array(populations).flatten()
+    plot_df["population"] = np.array([grouped_ages_df.loc[grouped_ages_df.index.levels[1] == ccg].values[0]
+                                      for ccg in grouped_ages_df.index.levels[1]]).flatten()
 
     g = sns.relplot(x="year", y="population", hue="ccg", kind="line", data=plot_df, legend=False, height=5, aspect=3)
     group_name = group.replace("_", " ")
