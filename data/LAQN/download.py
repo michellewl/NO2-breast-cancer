a42e2de30cdafc047b0ade5b0a86fedf18d91d1a
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-04-24 18:38:07 +0100

    Confirmed that I can call for 2002-18 data for one species, one site, without issues.

--- data/LAQN/download.py
+++ data/LAQN/download.py
@@ -12,4 +12,16 @@ import pandas as pd
 london_sites = requests.get("http://api.erg.kcl.ac.uk/AirQuality/Information/MonitoringSites/GroupName=London/Json")
 
 london_sites_df = pd.DataFrame(london_sites.json()['Sites']['Site'] )
-print(london_sites_df.head)
+#print(london_sites_df.columns)
+all_site_codes = london_sites_df["@SiteCode"].tolist()
+#print(all_site_codes)
+
+# ### Example to pull data from a single site
+# pandas is clever enough to do this all for you!
+SiteCode = "BG1"
+SpeciesCode = "NO2"
+StartDate = "2002-01-01"
+EndDate = "2018-01-01"
+single_site = pd.read_csv(f"http://api.erg.kcl.ac.uk/AirQuality/Data/SiteSpecies/"
+                          f"SiteCode={SiteCode}/SpeciesCode={SpeciesCode}/StartDate={StartDate}/EndDate={EndDate}/csv")
+print(single_site)
