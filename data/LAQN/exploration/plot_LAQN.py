4f38098cd6fd9896b5b6b9d4ed91a2df6dd351bd
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-04-26 23:05:18 +0100

    Speed up script by making a smaller sample dataframe to call.

--- data/LAQN/exploration/plot_LAQN.py
+++ data/LAQN/exploration/plot_LAQN.py
@@ -9,8 +9,7 @@ sns.set(style="darkgrid")
 folder = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) # folder where data is saved
 filename = [file for file in os.listdir(folder) if "all_sites.csv" in file][0]
 no2_df = pd.read_csv(os.path.join(folder, filename))
-no2_df.MeasurementDateGMT = pd.to_datetime(no2_df.MeasurementDateGMT)
-
+no2_df.set_index("MeasurementDateGMT", inplace=True)
 
 # site_strings = no2_df.columns[2].split()
 # site_name = ' '.join([str(elem) for elem in site_strings[:site_strings.index("-")]])
@@ -23,10 +22,15 @@ no2_df.MeasurementDateGMT = pd.to_datetime(no2_df.MeasurementDateGMT)
 # meta_data_df = meta_data_df.loc[:,["@LocalAuthorityName", "@SiteName"]]
 # print(meta_data_df)
 
+random_seed = 6
 
-for column in no2_df.sample(frac=0.1, axis="columns").iloc[:, 1:].columns:
+sample_df = no2_df.sample(frac=0.1, axis="columns", random_state=random_seed)
+sample_df.reset_index(inplace=True)
+sample_df.MeasurementDateGMT = pd.to_datetime(sample_df.MeasurementDateGMT)
+
+for column in sample_df.iloc[:, 1:].columns:
     print(f"Plotting {column}...")
-    g = sns.relplot(x="MeasurementDateGMT", y=column, kind="line", data=no2_df, legend=True, height=5, aspect=3)
+    g = sns.relplot(x="MeasurementDateGMT", y=column, kind="line", data=sample_df, legend="full", height=5, aspect=3)
 
 g.fig.suptitle(f"LAQN NO2")
 plt.show()
