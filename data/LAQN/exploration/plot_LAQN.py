475f2c42183e780be616dff1816c83715cdc400e
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-04-26 23:46:43 +0100

    Changed from seaborn plotting to matplotlib plotting (but keeping seaborn formatting because it looks nice) because it's easier to manipulate the dataset.

--- data/LAQN/exploration/plot_LAQN.py
+++ data/LAQN/exploration/plot_LAQN.py
@@ -23,14 +23,23 @@ no2_df.set_index("MeasurementDateGMT", inplace=True)
 # print(meta_data_df)
 
 random_seed = 6
+sample_fraction = 0.1
 
-sample_df = no2_df.sample(frac=0.1, axis="columns", random_state=random_seed)
+sample_df = no2_df.sample(frac=sample_fraction, axis="columns", random_state=random_seed)
 sample_df.reset_index(inplace=True)
 sample_df.MeasurementDateGMT = pd.to_datetime(sample_df.MeasurementDateGMT)
 
+fig, ax = plt.subplots(figsize=(20, 8))
+fig.suptitle(r"LAQN sample - NO$_2$")
 for column in sample_df.iloc[:, 1:].columns:
     print(f"Plotting {column}...")
-    g = sns.relplot(x="MeasurementDateGMT", y=column, kind="line", data=sample_df, legend="full", height=5, aspect=3)
+    ax.plot(sample_df.MeasurementDateGMT, sample_df[column], label=column, alpha=0.3)
 
-g.fig.suptitle(f"LAQN NO2")
-plt.show()
+plt.figtext(0.2, 0.8, f"Sampling {int(sample_fraction*100)}% of all sites with random seed ({random_seed})")
+ax.set(xlabel="Time", ylabel=r"NO$_2$ ($\mu$g m$^{-3}$)")
+ax.legend(loc="upper right")
+# plt.show()
+
+plot_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), f"LAQN_NO2_sample_seed{random_seed}.png")
+fig.savefig(plot_filename)
+print(f"\nSaved plot to:\n{plot_filename}")
