0c5c0b86b657b1127cd47e42c52ac58b3caae11a
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-04-28 18:11:28 +0100

    Added code to produce new dataframe mapping each LAQN site onto local authority and CCG.

--- data/LAQN/processing/site_averaging.py
+++ data/LAQN/processing/site_averaging.py
@@ -14,8 +14,9 @@ folder = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) # folder w
 
 meta_data = requests.get("http://api.erg.kcl.ac.uk/AirQuality/Information/MonitoringSites/GroupName=London/Json")
 meta_data_df = pd.DataFrame(meta_data.json()['Sites']['Site'])
-# meta_data_df = meta_data_df.loc[:,["@LocalAuthorityName", "@SiteName"]]
-local_auths = list(set(meta_data_df["@LocalAuthorityName"].tolist()))
+meta_data_df = meta_data_df.loc[:,["@LocalAuthorityName", "@SiteName"]]
+meta_data_df.columns = ["local_authority", "site_name"]
+local_auths = list(set(meta_data_df["local_authority"].tolist()))
 local_auths.sort()
 print(f"{len(local_auths)} local authorities.")
 
@@ -23,18 +24,22 @@ ccg_folder = os.path.join(os.path.dirname(folder), "CCG_populations")
 ccg_filename = [file for file in os.listdir(ccg_folder) if "london_females_2002-18.csv" in file][0]
 ccg_df = pd.read_csv(os.path.join(ccg_folder, ccg_filename))
 ccgs = [ccg.replace("NHS ", "") for ccg in list(set(ccg_df["ccg"].tolist()))]
-ccgs = [ccg.replace("&", "and") for ccg in ccgs]
 print(f"{len(ccgs)} CCGs.")
 
 odd_sites = [site for site in local_auths if site not in ccgs]
 matched_sites = [site for site in local_auths if site in ccgs]
 print(f"\n{len(odd_sites)} LAQN local authorities that don't match CCG names:\n{odd_sites}")
 
-site_mapping_df = pd.DataFrame(list(zip(matched_sites, matched_sites)), columns = ["LAQN", "CCG"])
+la_to_ccg_df = pd.DataFrame(list(zip(matched_sites, matched_sites)), columns=["local_authority", "ccg"])
 
 for site in odd_sites:
     for ccg in ccgs:
         if re.compile(site.split()[0]).findall(ccg):
-            df = pd.DataFrame([(site, ccg)], columns=["LAQN", "CCG"])
-            site_mapping_df = site_mapping_df.append(df, ignore_index=True)
-print(f"\nCheck sites have been mapped correctly:\n{site_mapping_df.tail(len(odd_sites))}")
+            df = pd.DataFrame([(site, ccg)], columns=["local_authority", "ccg"])
+            la_to_ccg_df = la_to_ccg_df.append(df, ignore_index=True)
+print(f"\nCheck sites have been mapped correctly:\n{la_to_ccg_df.tail(len(odd_sites))}")
+
+site_mapping_df = meta_data_df.merge(la_to_ccg_df, on="local_authority")
+
+ccg = "Barking & Dagenham"
+# Try processing on one ccg first!
