7e7e289ffff5dc1189e4a4614e3732af93057c6e
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-05-04 12:29:40 +0100

    Adding code to one-hot encode categorical variables and rename columns.

--- data/NCRAS/processing/functions.py
+++ data/NCRAS/processing/functions.py
@@ -28,3 +28,39 @@ def load_df_from_xlsheet(filename, sheet_name_pattern, verbose=False):
             print(f"Sheet name: {required_sheet}")
 
         return pd.read_excel(filename, required_sheet)
+
+def get_sheet_names(filename, pattern=False):
+    """
+    Function to print sheet names in an Excel file.
+    :param filename: String object indicating path to Excel file.
+    :param pattern: re compiler object (optional) to specify sheet name pattern.
+    :return: List of sheet names as string objects.
+    """
+
+    workbook = xlrd.open_workbook(filename)
+    sheets = workbook.sheet_names()
+    if pattern:
+        required_sheets = []
+        for sheet in sheets:
+            if pattern.findall(sheet):
+                required_sheets.append(sheet)
+            else:
+                print("No sheets found matching this name.")
+                return None
+        return required_sheets
+    else:
+        return sheets
+
+def one_hot(df, columns):
+    '''
+    one-hot encode variables in a specified column
+    ----------
+    Parameters:
+    df (pandas dataframe)
+    columns (list of strings): columns to one hot encode
+    -------
+    Return:
+     One pandas dataframe with the one hot encoded columns
+    '''
+    new_df = pd.concat((df, pd.get_dummies(df[columns])), axis=1)
+    return new_df.drop(columns, axis=1)
--- data/NCRAS/processing/xl_to_csv.py
+++ data/NCRAS/processing/xl_to_csv.py
@@ -6,8 +6,22 @@ import numpy as np
 
 folder = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) # folder where data is downloaded
 files = [f for f in os.listdir(folder) if '.xls' in f.lower()] # lists filepaths for all Excel files (case-insensitive)
-print(files)
+print(f"Filename: {files}")
 
 filepath = os.path.join(folder, files[0])
-df = fn.load_df_from_xlsheet(filepath, re.compile(r"Data\w+"))
+
+sheets = fn.get_sheet_names(filepath)
+print(f"Excel sheets: {sheets}")
+
+df = fn.load_df_from_xlsheet(filepath, sheets[1])
+
+df.loc[:, "CCG_NAME"] = [ccg.replace(" CCG", "")for ccg in df.CCG_NAME]
+df = df.rename(columns={"BEHAVIOUR_CODE_DESC": "behaviour", "RANK_VAR": "rank"})
+print(df.columns)
+
+df = fn.one_hot(df, ["AGE_CAT", "BEHAVIOUR_CODE_DESC", "RANK_VAR"])
+df.columns = [column.lower() for column in df.columns]
+df.columns = [column.replace(" ", "_") for column in df.columns]
+
+print(df.columns)
 print(df)
