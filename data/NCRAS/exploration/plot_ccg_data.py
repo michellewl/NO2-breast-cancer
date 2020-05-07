859bcf7dcd3ed66cc108f3142d6fd4dd3ba3eb52
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-05-07 19:04:51 +0100

    Final per capita plots.

--- data/NCRAS/exploration/plot_ccg_data.py
+++ data/NCRAS/exploration/plot_ccg_data.py
@@ -18,15 +18,15 @@ cancer_df.date = pd.to_datetime(cancer_df.date)
 ccgs = cancer_df.ccg_name.unique()
 print(f"{len(ccgs)} CCGs.")
 
-timeseries, axs = plt.subplots(8, 4, figsize=(25, 20), sharex=True, sharey=True)
+timeseries, axs = plt.subplots(8, 4, figsize=(25, 15), sharex=True, sharey=True)
 print(f"Plotting timeseries...")
-timeseries.suptitle("Monthly fractional breast cancer cases in London CCGs")
+timeseries.suptitle("Monthly per capita breast cancer cases in London CCGs")
 count = 0
 for ax in axs.flat:
     ccg = ccgs[count]
     plot_df = cancer_df.loc[cancer_df.ccg_name==ccg]
-    ax.plot(plot_df.date, plot_df.all_ages, alpha=0.7, c=f"C{count}")
-    ax.set(xlabel="Time", ylabel="Cases as fraction of population (monthly)")
+    ax.plot(plot_df.date, plot_df.all_ages, alpha=0.7, c=f"C{count+1}")
+    ax.set(xlabel="Time", ylabel="per capita")
     ax.label_outer()
     ax.set_title(ccg)
     count += 1
@@ -36,13 +36,13 @@ timeseries.show()
 
 histogram, axs = plt.subplots(8, 4, figsize=(25, 20), sharex=True, sharey=True)
 print("Plotting histograms...")
-histogram.suptitle("Monthly fractional breast cancer cases in London CCGs")
+histogram.suptitle("Monthly per capita breast cancer cases in London CCGs")
 count = 0
 for ax in axs.flat:
     ccg = ccgs[count]
     plot_df = cancer_df.loc[cancer_df.ccg_name == ccg]
-    ax.hist(plot_df.all_ages, alpha=0.7, color=f"C{count}")
-    ax.set(xlabel="Cases as fraction of population (monthly)")
+    ax.hist(plot_df.all_ages, alpha=0.7, color=f"C{count+1}")
+    ax.set(xlabel="per capita cases (monthly)")
     ax.label_outer()
     ax.set_title(ccg)
     count += 1
@@ -51,11 +51,11 @@ histogram.show()
 
 print("Saving plots...")
 
-plot_filename = f"monthly_fractional_cases_ccg_timeseries.png"
+plot_filename = f"monthly_per_capita_cases_ccg_timeseries.png"
 timeseries.savefig(os.path.join(os.path.dirname(os.path.realpath(__file__)), plot_filename))
 print(f"\nSaved {plot_filename}")
 
-plot_filename = f"monthly_fractional_cases_ccg_histograms.png"
+plot_filename = f"monthly_per_capita_cases_ccg_histograms.png"
 histogram.savefig(os.path.join(os.path.dirname(os.path.realpath(__file__)), plot_filename))
 print(f"\nSaved {plot_filename}")
 
