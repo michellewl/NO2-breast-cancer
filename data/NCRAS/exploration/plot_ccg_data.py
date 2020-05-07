08249d4094ca6fe711a3278f405ece47898c0642
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-05-07 18:56:28 +0100

    Updated script to use scaled case fractions.

--- data/NCRAS/exploration/plot_ccg_data.py
+++ data/NCRAS/exploration/plot_ccg_data.py
@@ -8,25 +8,25 @@ sns.set(style="darkgrid")
 
 
 folder = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) # folder where data is downloaded
-files = [f for f in os.listdir(folder) if "ccgs.csv" in f.lower()] # lists filepaths (case-insensitive)
+files = [f for f in os.listdir(folder) if "ccgs_population_fraction.csv" in f.lower()] # lists filepaths (case-insensitive)
 print(f"Filename: {files}")
 filepath = os.path.join(folder, files[0])
 
 cancer_df = pd.read_csv(filepath)
-cancer_df.diagnosis_date = pd.to_datetime(cancer_df.diagnosis_date)
+cancer_df.date = pd.to_datetime(cancer_df.date)
 
 ccgs = cancer_df.ccg_name.unique()
-print(len(ccgs))
+print(f"{len(ccgs)} CCGs.")
 
 timeseries, axs = plt.subplots(8, 4, figsize=(25, 20), sharex=True, sharey=True)
 print(f"Plotting timeseries...")
-timeseries.suptitle("Monthly breast cancer cases in London CCGs")
+timeseries.suptitle("Monthly fractional breast cancer cases in London CCGs")
 count = 0
 for ax in axs.flat:
     ccg = ccgs[count]
     plot_df = cancer_df.loc[cancer_df.ccg_name==ccg]
-    ax.plot(plot_df.diagnosis_date, plot_df.all_ages, alpha=0.7, c=f"C{count}")
-    ax.set(xlabel="Time", ylabel="Number of cases (monthly)")
+    ax.plot(plot_df.date, plot_df.all_ages, alpha=0.7, c=f"C{count}")
+    ax.set(xlabel="Time", ylabel="Cases as fraction of population (monthly)")
     ax.label_outer()
     ax.set_title(ccg)
     count += 1
@@ -36,13 +36,13 @@ timeseries.show()
 
 histogram, axs = plt.subplots(8, 4, figsize=(25, 20), sharex=True, sharey=True)
 print("Plotting histograms...")
-histogram.suptitle("Monthly breast cancer cases in London CCGs")
+histogram.suptitle("Monthly fractional breast cancer cases in London CCGs")
 count = 0
 for ax in axs.flat:
     ccg = ccgs[count]
     plot_df = cancer_df.loc[cancer_df.ccg_name == ccg]
     ax.hist(plot_df.all_ages, alpha=0.7, color=f"C{count}")
-    ax.set(xlabel="Monthly number of cases")
+    ax.set(xlabel="Cases as fraction of population (monthly)")
     ax.label_outer()
     ax.set_title(ccg)
     count += 1
@@ -51,11 +51,11 @@ histogram.show()
 
 print("Saving plots...")
 
-plot_filename = f"monthly_cases_ccg_timeseries.png"
+plot_filename = f"monthly_fractional_cases_ccg_timeseries.png"
 timeseries.savefig(os.path.join(os.path.dirname(os.path.realpath(__file__)), plot_filename))
 print(f"\nSaved {plot_filename}")
 
-plot_filename = f"monthly_cases_ccg_histograms.png"
+plot_filename = f"monthly_fractional_cases_ccg_histograms.png"
 histogram.savefig(os.path.join(os.path.dirname(os.path.realpath(__file__)), plot_filename))
 print(f"\nSaved {plot_filename}")
 
