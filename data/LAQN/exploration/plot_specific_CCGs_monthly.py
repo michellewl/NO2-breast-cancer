a645bba3203300bee933473e87175881bf46e5d5
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-05-11 15:21:45 +0100

    Create script for exploring specific sites.

---
+++ data/LAQN/exploration/plot_specific_CCGs_monthly.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="darkgrid")
+
+folder = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) # folder where data is saved
