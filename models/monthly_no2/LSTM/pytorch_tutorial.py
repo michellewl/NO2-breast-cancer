1077468577f9e118d421b5e160731a48b6cf8843
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-05-26 16:01:57 +0100

    Finish first part of PyTorch LSTM tutorial.

--- models/monthly_no2/LSTM/pytorch_tutorial.py
+++ models/monthly_no2/LSTM/pytorch_tutorial.py
@@ -20,5 +20,20 @@ for i in inputs:
     # Step through the sequence one element at a time
     # After each step, hidden contains the hidden state
     out, hidden = lstm(i.view(1, 1, -1), hidden)
+    # print(out)
+    # print(hidden)
 
-# Alternatively, we can do
+# Alternatively, we can do the entire sequence at once.
+# The first value returned by LSTM is all of the hidden states throughout the
+# sequence. The second value is the most recent hidden state (compare the last
+# slice of "out" with "hidden" below - they are the same).
+# This is because
+# "out" will give you access to all hidden states in the sequence
+# "hidden" will allow you to continue the sequence and backpropagate by
+# passing it as an argument to the LSTM at a later time.
+# Add the extra 2nd dimension
+inputs = torch.cat(inputs).view(len(inputs), 1, -1)
+hidden = (torch.randn(1, 1, 3), torch.randn(1, 1, 3))  # clean out hidden state
+out, hidden = lstm(inputs, hidden)
+print(out)
+print(hidden)
