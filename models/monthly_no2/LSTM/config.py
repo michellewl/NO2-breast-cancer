0c105615003251c0e6fd6bb7843b3d024c137944
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-06-05 13:14:00 +0100

    Ran LSTM model for various conditions.

--- models/monthly_no2/LSTM/config.py
+++ models/monthly_no2/LSTM/config.py
@@ -1,10 +1,10 @@
 training_window = 3  # consider the last X months of NO2 for each breast cancer diagnosis month
 
-aggregation = False  # Choose from ["mean"], or ["min", "max"]. Make this False if not using.
-quantile_step = 0.1  # Make this False if not using.
+aggregation = ["min", "max"]  # Choose from ["mean"], or ["min", "max"]. Make this False if not using.
+quantile_step = False  # Make this False if not using.
 
 ccgs = ["NHS Central London (Westminster)", "NHS Richmond"]
-ccg = ccgs[1]
+ccg = ccgs[0]
 test_year = 2017
 
 dates_as_inputs = False
@@ -13,7 +13,7 @@ age_category = "all_ages"
 
 hidden_layer_size = 100
 batch_size = 14
-num_epochs = 1000
+num_epochs = 5000
 batches_per_print = False
 epochs_per_print = 50
 random_seed = 1
