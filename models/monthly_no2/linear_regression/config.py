e8c4fc7a686d447bcc653110596370a350d403aa
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-06-19 18:28:50 +0100

    Reran with MSE calculation.

--- models/monthly_no2/linear_regression/config.py
+++ models/monthly_no2/linear_regression/config.py
@@ -8,12 +8,12 @@ training_window = False  # consider the last X months of NO2 for each breast can
 aggregation = False  # Choose from ["mean"], or ["min", "max"]. Make this False if not using.
 quantile_step = 0.1  # Make this False if not using.
 
-ccgs = ["clustered_ccgs"]  # ["clustered_ccgs"]  # ["NHS Richmond"] # ["all_ccgs"]
+ccgs = ["all_ccgs"]  # ["clustered_ccgs"]  # ["NHS Richmond"] # ["all_ccgs"]
 cluster_label = 1
 n_clusters = 2
 cluster_variables = "both_ncras_no2"
 
 test_year = 2017
 
-age_category = "age_cat_40-69"  # "age_cat_>=70"  #"age_cat_<40"  #"age_cat_40-69"  #"all_ages"
+age_category = "age_cat_>=70"  # "age_cat_>=70"  #"age_cat_<40"  #"age_cat_40-69"  #"all_ages"
 
