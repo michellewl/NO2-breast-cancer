a7af064c52d6484a10119c9f118f4d716f9480d6
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-07-15 00:11:50 +0100

    Experiments.

--- models/monthly_no2/LSTM/config.py
+++ models/monthly_no2/LSTM/config.py
@@ -2,7 +2,7 @@ SpeciesCode = "NO2"
 laqn_start_date = "1997-01-01"
 laqn_end_date = "2018-01-01"
 
-training_window = 12  # consider the last X months of NO2 for each breast cancer diagnosis month
+training_window = 60  # consider the last X months of NO2 for each breast cancer diagnosis month
 
 aggregation = False  # Choose from ["mean"], or ["min", "max"]. Make this False if not using.
 quantile_step = 0.1  # Make this False if not using.
@@ -14,7 +14,7 @@ cluster_variables = "both_ncras_no2"
 
 test_year = 2017
 
-age_category = "all_ages"  # "age_cat_>=70"  #"age_cat_<40"  #"age_cat_40-69"  #"all_ages"
+age_category = "age_cat_<40"  # "age_cat_>=70"  #"age_cat_<40"  #"age_cat_40-69"  #"all_ages"
 
 hidden_layer_size = 4
 batch_size = 30
@@ -28,5 +28,5 @@ learning_rate = 0.001
 model_epoch = "best"  # Choose "final" or "best" model.
 
 compute_test_loss = True
-noise_standard_deviation = 0.3  # Standard deviation of Gaussian noise
-include_plot_title = False
+noise_standard_deviation = 0.3  # Standard deviation of Gaussian noise (been using 0.3)
+include_plot_title = True
