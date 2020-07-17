7719a9bdf2f8ec0b8b920cc8de8b39c9548d7612
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-07-17 17:31:02 +0100

    Added code to calculate MAPE metric and add to plots.

--- models/monthly_no2/LSTM/functions.py
+++ models/monthly_no2/LSTM/functions.py
@@ -9,3 +9,10 @@ def create_x_sequences(x_array, num_sequences, tw):
         input_sequences.append(train_sequence)
     input_sequences = np.stack(input_sequences, axis=0)
     return input_sequences
+
+def mape_score(targets, predictions):
+    zero_indices = np.where(targets == 0)
+    targets_drop_zero = np.delete(targets, zero_indices)
+    prediction_drop_zero = np.delete(predictions, zero_indices)
+    mape = np.sum(np.abs(targets_drop_zero - prediction_drop_zero)/targets_drop_zero) * 100/len(targets_drop_zero)
+    return mape
